2f8065020d8d17e02bf449f84564e2ecad3cfc9c
Nguyen Thanh Phong <phong3nt@gmail.com>
2025-10-05 23:57:53 +0700

    FEAT: Implement question answer logic. Generate 3 unique incorrect answers, shuffle all 4 options, and assign text to answer buttons.

--- Assets/Scripts/QuizManager.cs
+++ Assets/Scripts/QuizManager.cs
@@ -1,12 +1,19 @@
 ﻿using UnityEngine;
 using TMPro;
+using UnityEngine.UI; 
+using System.Collections.Generic; 
+
 
 public class QuizManager : MonoBehaviour
 {
     public TextMeshProUGUI questionText;
     public TextMeshProUGUI scoreText;
-    private int score;
 
+    public Button[] answerButtons;
+
+
+    private int score;
+    private int correctAnswer;
     public QuestionData[] questions;
 
     void Start()
@@ -22,11 +29,44 @@ public class QuizManager : MonoBehaviour
     {
         int numberA = Random.Range(1, 11);
         int numberB = Random.Range(1, 11);
-        int correctAnswer = numberA + numberB;
+        correctAnswer = numberA + numberB; 
 
         questionText.text = $"{numberA} + {numberB} = ?";
 
-        Debug.Log("New question: " + questionText.text + " - Answer: " + correctAnswer);
+        List<int> answers = new List<int>();
+        answers.Add(correctAnswer);
+
+        int incorrectAnswerCount = 0;
+        while (incorrectAnswerCount < 3)
+        {
+            int randomOffset = Random.Range(-5, 6);
+            int incorrectAnswer = correctAnswer + randomOffset;
+
+            if (incorrectAnswer > 0 && !answers.Contains(incorrectAnswer))
+            {
+                answers.Add(incorrectAnswer);
+                incorrectAnswerCount++;
+            }
+        }
+
+        Shuffle(answers);
+
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = answers[i].ToString();
+        }
+    }
+
+    void Shuffle(List<int> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            int temp = list[i];
+            int randomIndex = Random.Range(i, list.Count);
+            list[i] = list[randomIndex];
+            list[randomIndex] = temp;
+        }
     }
 
 
